dfa53df4599536774a9e94738892f808a705f97e
Al Rafi <60866655+Rafi1999@users.noreply.github.com>
2023-09-22 19:04:51 +0600

    adding

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import loginImage from '../assets/image/login.svg';
 import { useForm, useWatch } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { createUser } from '../redux/features/user/userSlice';
 
 const Signup = () => {
   const { handleSubmit, register, control } = useForm();
@@ -9,7 +11,7 @@ const Signup = () => {
   const confirmPassword = useWatch({ control, name: 'confirmPassword' });
   const navigate = useNavigate();
   const [disabled, setDisabled] = useState(true);
-
+  const dispatch = useDispatch();
   useEffect(() => {
     if (
       password !== undefined &&
@@ -26,7 +28,7 @@ const Signup = () => {
 
   const onSubmit = ({ name, email, password }) => {
     // Email Password signup
-    console.log(name, email, password);
+    dispatch(createUser({name, email, password}))
   };
 
   const handleGoogleLogin = () => {
